88b539d8ead2301fde5d0f56868c77f0781518b1
JoseEspinosaTello <jet5588@outlook.com>
2022-06-20 02:12:41 -0500

    update

---
+++ Queries/Employee_Database_challenge.sql
@@ -0,0 +1,68 @@
+-- Retrieve the emp_no, first_name, and last_name columns from the Employees table.
+SELECT emp_no, first_name, last_name
+FROM employees
+
+--	2. Retrieve the title, from_date, and to_date columns from the Titles table.
+SELECT title, from_date, to_date
+FROM titles
+
+--Create a new table using the INTO clause.
+--Join both tables on the primary key.
+--Filter the data on the birth_date column to retrieve the employees who were born between 1952 and 1955.
+--Order by the employee number.
+DROP TABLE if EXISTS retirement_titles;
+
+SELECT e.emp_no,
+		e.first_name,
+		e.last_name,
+		ti.title,
+		ti.from_date,
+		ti.to_date
+INTO retirement_titles
+FROM employees as e
+INNER JOIN titles as ti on e.emp_no = ti.emp_no
+WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+ORDER BY e.emp_no;
+
+
+-- Use Dictinct with Orderby to remove duplicate rows
+DROP TABLE if EXISTS unique_titles;
+
+SELECT DISTINCT ON (emp_no) emp_no,
+	first_name,
+	last_name,
+	title
+INTO unique_titles
+FROM retirement_titles
+WHERE (to_date = '9999-01-01')
+ORDER BY emp_no ASC, to_date DESC;
+
+-- retrieve the number of employees by their most recent job title who are about to retire.
+--retrieve the number of titles from the Unique Titles table
+--create a Retiring Titles table to hold the required information
+--Group the table by title, then sort the count column in descending order
+
+DROP TABLE IF EXISTS retiring_titles;
+
+SELECT count(emp_no), title
+INTO retiring_titles
+FROM unique_titles
+GROUP BY title
+ORDER BY count(emp_no) DESC;
+
+
+--Mentorship Eligibility table
+DROP TABLE IF EXISTS mentorship_eligibility;
+
+SELECT DISTINCT ON (e.emp_no) e.emp_no, e.first_name,
+		e.last_name,
+		e.birth_date,
+		de.from_date,
+		de.to_date,
+		ti.title
+INTO mentorship_eligibility
+FROM employees as e 
+INNER JOIN  dept_emp as de on e.emp_no = de.emp_no
+INNER JOIN titles as ti on e.emp_no = ti.emp_no
+WHERE (de.to_date = '9999-01-01') AND (e.birth_date BETWEEN '1965-01-01' and '1965-12-31')
+ORDER BY e.emp_no;
--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # Pewlett-Hackard-Analysis
+
+## Overview of the analysis:
+
+Pewlet-Hackard is preparing for the "silver tsunami'. This event forsees a large number of Pewlet-Hackard current employees retiring in the forseeable future. Pewlet-Hackard has request a list of employees eligible for retirment. Additionally, Pewlet-Hackard wants to identify the following wave  of retirees an imploy a mentorship program. This mentorship program will help new employee adapt, by using the experienced next wave retirees as their mentors.
+- we will create a set of queirs that will provide the names of employees eligible for retirment, count of employees retiring from each title, and employees eligible for the mentorship program.
+
+The purpose of the new analysis is well defined. (3 pt)
+Results:
+
+There is a bulleted list with four major points from the two analysis deliverables. (6 pt)
+Summary:
+
+The summary addresses the two questions and contains two additional queries or tables that may provide more insight. (5 pt)
